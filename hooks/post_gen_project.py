6efdf60b9c27e863319544fdd2b17a2ca4e422c7
Carlos de la Guardia <cguardia@yahoo.com>
2016-06-02 19:32:12 -0500

    initial commit

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,52 @@
+""" Enable venv and show convenience message """
+import subprocess
+import sys
+
+from textwrap import dedent
+
+try:
+    # python 3.2+
+    import venv
+    VIRTUALENV_AVAILABLE = True
+except ImportError:
+    VIRTUALENV_AVAILABLE = False
+
+
+if VIRTUALENV_AVAILABLE:
+    venv.create('.', with_pip=True)
+    proc = subprocess.Popen(
+            ['bin/pip', 'install', '--upgrade', 'pip', 'setuptools'],
+            shell=sys.platform.startswith('win'),
+            cwd='.'
+    )
+    proc.wait()
+    proc = subprocess.Popen(
+            ['bin/pip', 'install', '-e', '.'],
+            shell=sys.platform.startswith('win'),
+            cwd='.'
+    )
+    proc.wait()
+    proc = subprocess.Popen(
+            ['bin/initialize_{{ cookiecutter.repo_name }}_db', 'development.ini'],
+            shell=sys.platform.startswith('win'),
+            cwd='.'
+    )
+    proc.wait()
+
+separator = "=" * 79
+msg = dedent(
+    """
+    %(separator)s
+    Tutorials: http://docs.pylonsproject.org/projects/pyramid_tutorials
+    Documentation: http://docs.pylonsproject.org/projects/pyramid
+    Twitter (tips & updates): http://twitter.com/pylons
+    Mailing List: http://groups.google.com/group/pylons-discuss
+    Welcome to Pyramid.  Sorry for the convenience.
+    %(separator)s
+""" % {'separator': separator})
+
+if VIRTUALENV_AVAILABLE:
+    msg += "\nTo run the generated application, cd to %s and run:" % (
+        '{{ cookiecutter.repo_name }}')
+    msg += "\nbin/pserve development.ini"
+print(msg)
---
+++ {{cookiecutter.repo_name}}/README.txt
@@ -0,0 +1,13 @@
+{{ cookiecutter.project_name }}
+===============================
+
+Getting Started
+---------------
+
+- cd <directory containing this file>
+
+  - $VENV/bin/pip install -e .
+
+  - $VENV/bin/initialize_{{project}}_db development.ini
+
+  - $VENV/bin/pserve development.ini
