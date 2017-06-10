2e976390e999bd5239f67e10a70ab014f23cc0f1
Steve Piercy <web@stevepiercy.com>
2017-06-10 12:06:00 -0700

    Use pyramid >= 1.9a

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(here, 'CHANGES.txt')) as f:
     CHANGES = f.read()
 
 requires = [
-    'pyramid',
+    'pyramid >= 1.9a',
     'pyramid_debugtoolbar',
     'pyramid_jinja2',
     'pyramid_retry',
