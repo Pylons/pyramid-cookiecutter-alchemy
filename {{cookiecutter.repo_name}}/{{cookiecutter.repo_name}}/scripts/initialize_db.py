d7580cf90648ed6bfa9561f5a40b9d40573da0be
Michael Merickel <michael@merickel.org>
2018-08-04 22:00:11 -0500

    add extra help

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/scripts/initialize_db.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/scripts/initialize_db.py
@@ -8,13 +8,20 @@ from .. import models
 
 
 def setup_models(dbsession):
+    """
+    Add or update models / fixtures in the database.
+
+    """
     model = models.mymodel.MyModel(name='one', value=1)
     dbsession.add(model)
 
 
 def parse_args(argv):
     parser = argparse.ArgumentParser()
-    parser.add_argument('config_uri')
+    parser.add_argument(
+        'config_uri',
+        help='Configuration file, e.g., development.ini',
+    )
     return parser.parse_args(argv[1:])
 
 
